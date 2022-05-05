7fbd0374216caab06b9aff9acbb774394db36b2b
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-05-05 20:58:52 -0300

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
-             }
-  get '/member-data', to: 'members#show'
-  get '/show_plans', to: 'account_tier_types#index'
-  get '/show_timeable_objects', to: 'timeable_object#index'
-  get '/show_timeable_object_logs', to: 'timeable_object_log#index'
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+
+  resources :account_tier_types, path: :plans, only: :index, defaults: { format: :json }
+  resources :timeable_objects, path: :cards, only: %i[index update delete], defaults: { format: :json }
+  resources :timeable_object_logs, path: :timers, only: %i[index create], defaults: { format: :json }
+
   post '/update_user_info', to: 'users/config#update_user_info', defaults: { format: :json }
-  post '/create_timeable_object', to: 'timeable_object#create', defaults: { format: :json }
-  post '/create_timeable_object_log', to: 'timeable_object_log#create', defaults: { format: :json }
 end
